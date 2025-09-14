53aeec96091bb8b719581bf267de0e10b2a27836
Woolhorse <150866206+Woolhorse@users.noreply.github.com>
2025-09-14 13:41:18 -0700

    Finally fixed clerk

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,7 @@
+// app/sign-in/page.tsx
+"use client"
+import { SignIn } from "@clerk/nextjs"
+
+export default function SignInPage() {
+  return <SignIn />
+}
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,7 @@
+// app/sign-up/page.tsx
+"use client"
+import { SignUp } from "@clerk/nextjs"
+
+export default function SignUpPage() {
+  return <SignUp />
+}
