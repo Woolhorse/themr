4f1a4121f782d748c83f90970dd456c684836866
Woolhorse <150866206+Woolhorse@users.noreply.github.com>
2025-09-14 00:48:27 -0700

    Minor update

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 export default function Footer() {
   return (
     <footer className="p-4 border-t border-gray-800 text-gray-400 flex flex-col sm:flex-row justify-between items-center gap-2">
-      <p>©2025 Themr</p>
+      <p>©2025 Themr      Website by <Link href="https://discord.com/users/511574627714203648" target="_blank">@woolhorse</Link> </p>
       <div className="flex gap-4">
         <Link href="https://discord.gg/k59qbrbmCK" target="_blank">Discord</Link>
         <Link href="/privacy">Privacy Policy</Link>
